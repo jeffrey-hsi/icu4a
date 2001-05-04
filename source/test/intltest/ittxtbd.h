6b8e1c5066d4ff2cce7b774b6bd6d1a690db91d5
nobody <nobody@251d0590-4201-4cf1-90de-194747b24ca1>
2001-05-04 00:47:16 +0000

    This commit was manufactured by cvs2svn to create tag 'stable-2001-05-03'.

--- source/test/intltest/ittxtbd.h
+++ source/test/intltest/ittxtbd.h
@@ -76,10 +76,6 @@ public:
      * with the word before it
      **/
     void TestMaiyamok(void);
-     /**
-     * Test Thai word break using generalIteratorTest()
-     **/
-    void TestThaiWordBreak(void);
     /**
      * test behaviour of BreakIterator on an empty string
      **/
