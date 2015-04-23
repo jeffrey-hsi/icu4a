7816a917d6434af51b3d859ec38c2165bb796ec4
heninger <heninger@251d0590-4201-4cf1-90de-194747b24ca1>
2015-04-23 21:23:53 +0000

    ticket:11643 Thread tests, update copyright dates.

--- icu/branches/andy/11643/source/test/intltest/simplethread.cpp
+++ icu/branches/andy/11643/source/test/intltest/simplethread.cpp
@@ -1,6 +1,6 @@
 /********************************************************************
  * COPYRIGHT: 
- * Copyright (c) 1999-2013, International Business Machines Corporation and
+ * Copyright (c) 1999-2015, International Business Machines Corporation and
  * others. All Rights Reserved.
  ********************************************************************/
 
