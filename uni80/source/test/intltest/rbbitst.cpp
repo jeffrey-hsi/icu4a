4ecbee21f82b49d9d47f8977fd4bda0f72ae8ebe
heninger <heninger@251d0590-4201-4cf1-90de-194747b24ca1>
2015-05-01 01:08:07 +0000

    ticket:11574 RBBI Sentence and WordBreakTest files (d18), plus sentence rule update. All tests passing.

--- uni80/source/test/intltest/rbbitst.cpp
+++ uni80/source/test/intltest/rbbitst.cpp
@@ -2755,8 +2755,9 @@ int32_t RBBISentMonkey::next(int32_t prevPos) {
             continue;
         }
 
-        // Rule (7).  Upper ATerm  x  Uppper
-        if (fUpperSet->contains(c0) && fATermSet->contains(c1) && fUpperSet->contains(c2)) {
+        // Rule (7).  (Upper | Lower) ATerm  x  Uppper
+        if ((fUpperSet->contains(c0) || fLowerSet->contains(c0)) &&
+                fATermSet->contains(c1) && fUpperSet->contains(c2)) {
             continue;
         }
 
