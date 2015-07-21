d224418c7c6eec27704866ee7d464db60455eb4d
tkeep <tkeep@251d0590-4201-4cf1-90de-194747b24ca1>
2015-07-21 17:09:42 +0000

    ticket:11767: Create branch.

---
+++ 11767_memory/source/common/sharedobject.cpp
@@ -0,0 +1,95 @@
+/*
+******************************************************************************
+* Copyright (C) 2014, International Business Machines
+* Corporation and others.  All Rights Reserved.
+******************************************************************************
+* sharedobject.cpp
+*/
+#include "sharedobject.h"
+
+U_NAMESPACE_BEGIN
+
+SharedObject::~SharedObject() {}
+
+UnifiedCacheBase::~UnifiedCacheBase() {}
+
+void
+SharedObject::addRef(UBool fromWithinCache) const {
+    umtx_atomic_inc(&totalRefCount);
+
+    // Although items in use may not be correct immediately, it
+    // will be correct eventually.
+    if (umtx_atomic_inc(&hardRefCount) == 1 && cachePtr != NULL) {
+        if (fromWithinCache) {
+            cachePtr->incrementItemsInUse();
+        } else {
+            cachePtr->incrementItemsInUseWithLocking();
+        }
+    }
+}
+
+void
+SharedObject::removeRef(UBool fromWithinCache) const {
+    UBool decrimentItemsInUse = (umtx_atomic_dec(&hardRefCount) == 0);
+    UBool allReferencesGone = (umtx_atomic_dec(&totalRefCount) == 0);
+
+    // Although items in use may not be correct immediately, it
+    // will be correct eventually.
+    if (decrimentItemsInUse && cachePtr != NULL) {
+        if (fromWithinCache) {
+            cachePtr->decrementItemsInUse();
+        } else {
+            cachePtr->decrementItemsInUseWithLockingAndEviction();
+        }
+    }
+    if (allReferencesGone) {
+        delete this;
+    }
+}
+
+void
+SharedObject::addSoftRef() const {
+    umtx_atomic_inc(&totalRefCount);
+    umtx_atomic_inc(&softRefCount);
+}
+
+void
+SharedObject::removeSoftRef() const {
+    umtx_atomic_dec(&softRefCount);
+    if (umtx_atomic_dec(&totalRefCount) == 0) {
+        delete this;
+    }
+}
+
+UBool
+SharedObject::allSoftReferences() const {
+    return umtx_loadAcquire(hardRefCount) == 0;
+}
+
+UBool
+SharedObject::allHardReferences() const {
+    return umtx_loadAcquire(softRefCount) == 0;
+}
+int32_t
+SharedObject::getRefCount() const {
+    return umtx_loadAcquire(totalRefCount);
+}
+
+int32_t
+SharedObject::getSoftRefCount() const {
+    return umtx_loadAcquire(softRefCount);
+}
+
+int32_t
+SharedObject::getHardRefCount() const {
+    return umtx_loadAcquire(hardRefCount);
+}
+
+void
+SharedObject::deleteIfZeroRefCount() const {
+    if(getRefCount() == 0) {
+        delete this;
+    }
+}
+
+U_NAMESPACE_END
