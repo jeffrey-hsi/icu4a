2ecd893ecc8f334a3ec6fe6c8de43ea441cb038a
heninger <heninger@251d0590-4201-4cf1-90de-194747b24ca1>
2015-10-12 02:16:50 +0000

    ticket:11961 Finer grained multithreading in TimeZoneFormatTest::TestTimeRoundTrip.

--- icu/branches/andy/11961/source/test/intltest/tzfmttst.h
+++ icu/branches/andy/11961/source/test/intltest/tzfmttst.h
@@ -15,6 +15,7 @@
 #include "intltest.h"
 
 class TimeZoneFormatTest : public IntlTest {
+  public:
     // IntlTest override
     void runIndexedTest(int32_t index, UBool exec, const char*& name, char* par);
 
@@ -24,6 +25,8 @@ class TimeZoneFormatTest : public IntlTest {
     void TestISOFormat(void);
     void TestFormat(void);
     void TestFormatTZDBNames(void);
+
+    void RunTimeRoundTripTests(int32_t threadNumber);
 };
 
 #endif /* #if !UCONFIG_NO_FORMATTING */
