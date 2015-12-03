d432f3ac76b1bc5ef885e19de766c4080edacbd7
heninger <heninger@251d0590-4201-4cf1-90de-194747b24ca1>
2015-12-03 20:15:06 +0000

    ticket:11961 Threaded tests, work in progress.

--- icu/branches/andy/11961/source/test/intltest/simplethread.h
+++ icu/branches/andy/11961/source/test/intltest/simplethread.h
@@ -26,19 +26,32 @@ class U_EXPORT SimpleThread
 
 class IntlTest;
 
+// ThreadPool - utililty class to simplify the spawning a group of threads by
+//              a multi-threaded test.
+//
+//   Usage: from within an intltest test function,
+//       ThreadPool<TestClass> pool(this,              // The current intltest test object,
+//                                                     //     of type "TestClass *"
+//                                  numberOfThreads,   // How many threads to spawn.
+//                                  TestClass::func);  // The function to be run by each thread.
+//                                                     //     It takes one int32_t parameter which
+//                                                     //     is set to the thread number, 0 to numberOfThreads-1.
+//
+//       pool.start();          // Start all threads running.
+//       pool.join();           // Wait until all threads have terminated.
+
 class ThreadPoolBase {
   public:
-    ThreadPoolBase(IntlTest *test, int32_t howMany);
+    ThreadPoolBase(IntlTest *test, int32_t numThreads);
     virtual ~ThreadPoolBase();
     
     void start();
     void join();
 
+  protected:
     virtual void callFn(int32_t param) = 0;
-    void initThreads();
-
     friend class ThreadPoolThread;
-  private:
+
     IntlTest  *fIntlTest;
     int32_t  fNumThreads;
     SimpleThread **fThreads;
@@ -48,15 +61,15 @@ class ThreadPoolBase {
 template<class TestClass>
 class ThreadPool : public ThreadPoolBase {
   private:
-    TestClass *fTest;
     void (TestClass::*fRunFnPtr)(int32_t);
   public:
     ThreadPool(TestClass *test, int howMany, void (TestClass::*runFnPtr)(int32_t threadNumber)) :
-        ThreadPoolBase(test, howMany), fTest(test), fRunFnPtr(runFnPtr) {initThreads(); };
+        ThreadPoolBase(test, howMany), fRunFnPtr(runFnPtr) {};
     virtual ~ThreadPool() {};
-  protected:
+  private:
     virtual void callFn(int32_t param) {
-        (fTest->*fRunFnPtr)(param);
+        TestClass *test = dynamic_cast<TestClass *>(fIntlTest);
+        (test->*fRunFnPtr)(param);
     }
 };
 #endif
